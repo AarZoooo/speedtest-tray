15a779b62cdd29ad48f035d8b476a90e2f358ff8
Aarju Pal <me@aarju.dev>
2026-06-13 21:34:21 +0530

    feat: implement native macOS menu bar status item support and clean exit

--- internal/gui_wails/app.go
+++ internal/gui_wails/app.go
@@ -10,8 +10,10 @@ import (
 
 // App represents the Wails application binding
 type App struct {
-	ctx    context.Context
-	cancel context.CancelFunc
+	ctx           context.Context
+	cancel        context.CancelFunc
+	windowVisible bool
+	MacIcon       []byte
 }
 
 // NewApp creates a new App instance
@@ -22,6 +24,12 @@ func NewApp() *App {
 // Startup initializes the app on Wails startup
 func (a *App) Startup(ctx context.Context) {
 	a.ctx = ctx
+	a.initMacStatusItem()
+}
+
+// Shutdown performs cleanup tasks when the application is closing
+func (a *App) Shutdown(ctx context.Context) {
+	a.cleanupMacStatusItem()
 }
 
 // ShowWindow displays the main window
@@ -35,12 +43,23 @@ func (a *App) ShowWindow() {
 	wailsRuntime.WindowUnminimise(a.ctx)
 	a.ApplyRoundedCorners()
 	wailsRuntime.EventsEmit(a.ctx, "window_shown")
+	a.windowVisible = true
 }
 
 // HideWindow hides the main window
 func (a *App) HideWindow() {
 	if a.ctx != nil {
 		wailsRuntime.WindowHide(a.ctx)
+		a.windowVisible = false
+	}
+}
+
+// ToggleWindow toggles the main window visibility
+func (a *App) ToggleWindow() {
+	if a.windowVisible {
+		a.HideWindow()
+	} else {
+		a.ShowWindow()
 	}
 }
 
---
+++ internal/gui_wails/tray_windows.go
@@ -0,0 +1,47 @@
+//go:build windows
+
+package gui_wails
+
+import (
+	"github.com/energye/systray"
+	"speedtest-tray/internal/config"
+)
+
+func StartTray(app *App, iconBytes []byte, macIconBytes []byte, appConfig *config.CustomConfig, toggleLogging func(bool)) {
+	go systray.Run(func() {
+		systray.SetTitle(config.AppName)
+		systray.SetTooltip(config.AppName)
+		systray.SetTemplateIcon(macIconBytes, iconBytes)
+		systray.SetOnClick(func(menu systray.IMenu) {
+			go app.ShowWindow()
+		})
+
+		show := systray.AddMenuItem("Show", "Show the speedtest window")
+		show.Click(app.ShowWindow)
+
+		systray.AddSeparator()
+
+		saveLogs := systray.AddMenuItemCheckbox("Enable Session Logging", "Save test logs to app data", appConfig.SaveLogs)
+		saveLogs.Click(func() {
+			if saveLogs.Checked() {
+				saveLogs.Uncheck()
+				toggleLogging(false)
+			} else {
+				saveLogs.Check()
+				toggleLogging(true)
+			}
+		})
+
+		systray.AddSeparator()
+
+		quit := systray.AddMenuItem("Quit", "Quit the application")
+		quit.Click(func() {
+			app.Quit()
+			systray.Quit()
+		})
+	}, func() {})
+}
+
+func (a *App) initMacStatusItem() {}
+
+func (a *App) cleanupMacStatusItem() {}
